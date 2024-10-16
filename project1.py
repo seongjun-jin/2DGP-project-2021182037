f76b203bad718f9f216977e8a0a163988e95f672
seongjun-jin <wlstjdwns2108@naver.com>
2024-10-16 14:23:42 +0900

    화면에 띄우기

--- project1.py
+++ project1.py
@@ -1,33 +1,48 @@
 from pico2d import *
-import random
-import os
 
 class Character:
     def __init__(self):
         self.x, self.y = 400,300
         self.frame = 0
-        self.action = 3
-        self.image = load_image("run_animation.png")
+        self.image = load_image("1.png")
+        if self.image is None:
+            print("Image failed to load")
 
     def handle_event(self):
-        pass
+        self.frame = 0
 
     def update(self):
         self.frame = (self.frame + 1) % 8
 
     def draw(self):
-        self.image.clip_draw(self.frame * 100, self.action * 100, 100, 100, self.x, self.y)
+        self.image.clip_draw(self.frame * 75, 2700, 75, 105, self.x, self.y)
 
 class BG:
     def __init__(self):
-        self.image = load_image("run_animation.png")
+        self.image = load_image("bg.png")
+        if self.image is None:
+            print("Image failed to load")
 
     def update(self):
         pass
 
     def draw(self):
-        self.image.draw(400, 30)
+        self.image.draw(400, 300)
 
+class Player:
+    def __init__(self):
+        self.x, self.y = 400, 30
+        self.frame = 0
+        self.image = load_image("player.png")
+
+    def handle_event(self):
+        self.frame = 0
+
+    def update(self):
+        self.frame = (self.frame + 1) % 2
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 20, 40, 20, 20, self.x, self.y)
 
 def handle_events():
     global running
@@ -44,30 +59,31 @@ def reset_world():
     global world
     global chara
     global bg
-
+    global player
     running = True
-
     world = []
 
+    player = Player()
     bg = BG()
     chara = Character()
 
     world.append(bg)
     world.append(chara)
+    world.append(player)
 
 def update_world():
     for o in world:
         o.update()
-    pass
 
 def render_world():
     clear_canvas()
     for o in world:
         o.draw()
-    update_world()
-    pass
+    update_canvas()
+
 
 open_canvas()
+
 
 reset_world()
 # game loop
